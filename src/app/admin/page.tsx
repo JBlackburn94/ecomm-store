faebf9ff3f3d0c34dcfbf1d415bc199333000bb5
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-06-15 18:10:28 +0100

    adds the admin layout along with connecting prisma to nextjs

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import { DashboardCard } from "@/components/DashboardCard";
+
+export default function AdminDashboard() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard title="Sales" subtitle="Description" body="Text" />
+    </div>
+  );
+}
---
+++ src/components/Nav.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Link from "next/link";
+import { ComponentProps } from "react";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+export function Nav({ children }: { children: React.ReactNode }) {
+  return (
+    <nav className="bg-primary text-primary-foreground flex justify-center px-4">
+      {children}
+    </nav>
+  );
+}
+
+export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
+  const pathName = usePathname();
+  return (
+    <Link
+      {...props}
+      className={cn(
+        "p-4 hover:bg-secondary hover:text-secondary-foreground focus-visible:hover:bg-secondary focus-visible:hover:text-secondary-foreground",
+        pathName === props.href && "bg-background text-foreground"
+      )}
+    ></Link>
+  );
+}
